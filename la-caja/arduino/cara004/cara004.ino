5143d5348809b29710d58def36c919a41553adef
rhyloo <jorge2@uma.es>
2022-12-16 10:19:54 +0100

    Placa bug solucionado
    Hay unas líneas que son muy importantes para el correcto
    funcionamiento, no estoy seguro de porqué, hay una teoría comentada en
    el archivo

--- la-caja/arduino/cara004/cara004.ino
+++ la-caja/arduino/cara004/cara004.ino
@@ -1,5 +1,5 @@
 /* Librería general  */
-//#include <infra.h>
+#include <infra.h>
 
 /* Libreria convertidor A/D ADS1015 */
 #include <Adafruit_ADS1X15.h>
@@ -16,41 +16,51 @@
 /* Librería de cosecha propia*/
 #include <Joystick.h>
 
-
 #define SEND_TIME_ 1
+
 #define x_channel 1
 #define y_channel 0
+#define SCREEN_WIDTH = 128; // OLED display width, in pixels
+#define SCREEN_HEIGHT = 64; // OLED display height, in pixels
 
-const int SCREEN_WIDTH = 128; // OLED display width, in pixels
-const int SCREEN_HEIGHT = 64; // OLED display height, in pixels
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT); // initialize the display:
+/* Initialize the display */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 Joystick joystick(x_channel,y_channel);
 
 String teststr = "rhyloo";
 
-void setup(void)
-{
-  Serial.begin(9600); 
-  // Iniciar el ADS1115
+void setup(void){
+  Serial.begin(9600);
+
   joystick.Setup();
+
+  /* Las siguientes lineas son muy importantes, desconozco el motivo, mi idea es
+     que si no las pones intenta el bus I2C más rápido de lo que puede y peta */
+  /****************************************************************************/
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Display setup failed");
+    while (true);
+  }
+  Serial.println("Display is good to go");
+  /****************************************************************************/
 }
 
 
 
-void loop(void)
-{
-  /* display.clearDisplay(); */
-  joystick.Loop();
-  /* if (Serial.available() > 0) { */
-  /*   teststr = Serial.readString();  //read until timeout */
-  /*   teststr.trim();                        // remove any \r \n whitespace at the end of the String */
-  /* } */
 
-  /* display.setTextSize(3); */
-  /* display.setTextColor(SSD1306_WHITE); */
-  /* display.setCursor(0, 20); */
-  /* display.print(teststr); */
-  /* display.display(); */
+void loop(void){
+  display.clearDisplay();
+  joystick.Loop();
+
+  if (Serial.available() > 0) {
+    teststr = Serial.readString();  //read until timeout
+    teststr.trim();                        // remove any \r \n whitespace at the end of the String
+  }
+
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 20);
+  display.print(teststr);
+  display.display();
 }
 
