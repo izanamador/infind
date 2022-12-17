cf6ea45f640326c0908d7f64ee90f9fe2c2fa01e
rhyloo <jorge2@uma.es>
2022-12-17 01:32:32 +0100

    Arquitectura infra añadida
    El modulo de la pantalla da problemas si la conexión serial se realiza
    rápido no puedo llamar a Infra antes porque se "bugea", la solución
    que he encontrado es ejecutar el código comentado en el setup, antes
    de invocar infra, otra posible solución es meter un delay o comprobar
    esa conexión desde infra directamente, pero si solo la necesita mi
    juego no veo motivos para implementar una función "compleja" en la
    clase compartida.

--- la-caja/arduino/cara004/cara004.ino
+++ la-caja/arduino/cara004/cara004.ino
@@ -1,5 +1,7 @@
-/* Librería general  */
-#include <infra.h>
+/* Librerías de uso generales*/
+#include <array>
+#include <string.h>
+#include <Wire.h>
 
 /* Libreria convertidor A/D ADS1015 */
 #include <Adafruit_ADS1X15.h>
@@ -8,15 +10,11 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 
-/* Librerías de uso generales*/
-#include <array>
-#include <string.h>
-#include <Wire.h>
+/* Librería general de la caja  */
+#include <infra.h>
 
-/* Librería de cosecha propia*/
+/* Librería de cosecha propia de la caja*/
 #include <Joystick.h>
-
-#define SEND_TIME_ 1
 
 #define x_channel 1
 #define y_channel 0
@@ -29,14 +27,35 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 /* Create joystick */
 Joystick joystick(x_channel,y_channel);
 
+/* Create objInfra */
+Infra objInfra;
+char *strTopicPub = "II3/ESP004/pub/cara004"; // topic principal para publicar contenido y lastwill
+char *strTopicCfg = "II3/ESP004/cfg/cara004"; // topic para recibir parametros de configuracion
+char *strTopicCmd = "II3/ESP004/cmd/cara004"; // topic para recibir peticiones de comando
+
 String teststr = "rhyloo";
 
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+  char *mensaje = (char *)malloc(length+1);
+  strncpy(mensaje, (char*)payload, length);
+  mensaje[length]='\0';
+  Serial.printf("Mensaje recibido [%s] %s\n", topic, mensaje);
+
+  if (strcmp(topic, strTopicCmd)==0)
+  {
+    if (mensaje[0] == '1') {
+        digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+      }
+      else
+      {
+        digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+      }
+  }
+  free(mensaje);
+}
+
 void setup(void){
-  Serial.begin(9600);
-
-  /* Initialize joystick*/
-  joystick.Setup();
-
   /****************************************************************************/
   /* Las siguientes lineas son muy importantes, desconozco el motivo, mi idea */
   /*    es que si no las pones intenta leer el bus I2C más rápido de lo que   */
@@ -49,12 +68,32 @@ void setup(void){
   }
   Serial.println("Display is good to go");
   /****************************************************************************/
+
+  /* setup de infrastructura */
+  objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CFG] = strTopicCfg;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
+  objInfra.objConfig["CRONO_INI"] ="30:00";
+  objInfra.objConfig["CRONO_MAX"] ="90:00";
+  objInfra.objConfig["CRONO_ADD"] ="10:00";
+  objInfra.objConfig["FREQ_START"]=1200;
+  objInfra.objConfig["FREQ_CARA"] =1300;
+  objInfra.objConfig["FREQ_FIN"]  =1400;
+  objInfra.objConfig["DURA_START"]=5000;
+  objInfra.objConfig["DURA_CARA"] =2000;
+  objInfra.objConfig["DURA_FIN"]  =10000;
+
+  objInfra.Setup(mqttCallback);
+
+  /* Initialize joystick*/
+  joystick.Setup();
 }
 
 
 void loop(void){
   static int direction = 0;
   display.clearDisplay();
+  objInfra.Loop();
   direction = joystick.Loop();
 
   if (Serial.available() > 0) {
