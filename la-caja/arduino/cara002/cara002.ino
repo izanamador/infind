050f57469a34e08bf9d14cb621069511f283fb51
rhyloo <jorge2@uma.es>
2022-12-27 20:40:47 +0100

    Cara002 demo funcional
    Contiene:
    - La clase infra y todas las necesarias para su correcto
    funcionamiento
    - Conexión con Node-Red y base de datos (local por ahora)
    - Conexión con dashboard y timers (a decidir)
    - Funciones extra para procesamiento de la información
    - Mecánicas del juego con el formato de FSM.
    - Envío constante del estado del juego
    - Bugs detectados:
      - Las cifras que empiezan por cero no las detecta bien, por ejemplo
      si una delas soluciones es "0002", el programa se buguearía hasta
      que el usuario introduzca una cifra "correcta"
    - Features:
      - Añadir pistas para el usuario:
        - Caliente o frío (facilita demasiado el juego, pista de nivel dificil)
        - Explicar el acertijo

--- la-caja/arduino/cara002/cara002.ino
+++ la-caja/arduino/cara002/cara002.ino
@@ -1,155 +1,178 @@
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+/* Librería general de la caja  */
+#include <infra.h>
 #include <ArduinoJson.h>
+
+/* Librerías de uso generales*/
+#include <array>
 #include <string.h>
+#include <Wire.h>
+#include <math.h>
+
+/* Libreria keypad */
 #include <Keypad.h>
-#include <iostream>
 
-
-const byte n_rows = 4;
-const byte n_cols = 4;
+#define n_rows 4
+#define n_cols 4
 
 char keys[n_rows][n_cols] = {
-{'1','2','3','A'},
-{'4','5','6','B'},
-{'7','8','9','C'},
-{'*','0','#','D'}
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
 
 byte rowPins[n_rows] = {D0, D1, D2, D3};
 byte colPins[n_cols] = {D4, D5, D6, D7};
 
-
-#define TOPIC_CONEXION_ "II3/ESP1/conexion"
-
-WiFiClient wClient;
-PubSubClient mqtt_client(wClient);
+/* Create keypad */
 Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, n_rows, n_cols);
 
-const char* ssid = "infind";
-const char* password = "1518wifi";
-const char* mqtt_server = "iot.ac.uma.es";
-const char* mqtt_user = "II3";
-const char* mqtt_pass = "qW30SImD";
+/* Create objInfra */
+Infra objInfra;
+char *strTopicPub = "II3/ESP002/pub/cara002"; // topic principal para publicar contenido y lastwill
+char *strTopicCfg = "II3/ESP002/cfg/cara002"; // topic para recibir parametros de configuracion
+char *strTopicCmd = "II3/ESP002/cmd/cara002"; // topic para recibir peticiones de comando
 
-// cadenas para topics e ID
-char ID_PLACA[16];
-char topic_PUB[256];
-char topic_SUB[256];
 
-// GPIOs
-int LED1 = 2;
-int LED2 = 16;
-int ans = -1;
+int game_ans = NULL;
+int state = 0;
 
-#define TAMANHO_MENSAJE 128
-unsigned long ultimo_mensaje=0;
-unsigned long ultimo_mensaje_dht=0;
-char mensaje[TAMANHO_MENSAJE];
-//-----------------------------------------------------
-void conecta_wifi() {
-  Serial.printf("\nConnecting to %s:\n", ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(200);
-    Serial.print(".");
-  }
-  Serial.printf("\nWiFi connected, IP address: %s\n", WiFi.localIP().toString().c_str());
-}
-
-//-----------------------------------------------------
-void conecta_mqtt() {
-  // Loop until we're reconnected
-  while (!mqtt_client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (mqtt_client.connect(ID_PLACA, mqtt_user, mqtt_pass)) {
-      Serial.printf(" conectado a broker: %s\n",mqtt_server);
-      mqtt_client.subscribe(TOPIC_CONEXION_);
-    } else {
-      Serial.printf("failed, rc=%d  try again in 5s\n", mqtt_client.state());
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-//-----------------------------------------------------
-void procesa_mensaje(char* topic, byte* payload, unsigned int length) {
-  char *mensaje = (char *)malloc(length+1); // reservo memoria para copia del mensaje
-  strncpy(mensaje, (char*)payload, length); // copio el mensaje en cadena de caracteres
-  mensaje[length]='\0'; // caracter cero marca el final de la cadena
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+  char *mensaje = (char *)malloc(length+1);
+  strncpy(mensaje, (char*)payload, length);
+  mensaje[length]='\0';
   Serial.printf("Mensaje recibido [%s] %s\n", topic, mensaje);
-  // compruebo el topic
 
-  //ans = 77;
-   if(strcmp(topic, TOPIC_CONEXION_)==0){
-     ans = atoi(mensaje);
-  }
-
-  Serial.println(ans);
-
+  if (strcmp(topic, strTopicCmd)==0)
+    {
+      game_ans = atoi(mensaje);
+    }
+  Serial.println(game_ans);
   free(mensaje);
 }
 
-//-----------------------------------------------------
-//     SETUP
-//-----------------------------------------------------
 void setup() {
-  Serial.begin(115200);
-  /* Serial.println(); */
-  Serial.println("Empieza setup...");
-  /* pinMode(LED1, OUTPUT);    // inicializa GPIO como salida */
-  /* pinMode(LED2, OUTPUT); */
-  /* digitalWrite(LED1, LOW); // apaga el led */
-  /* digitalWrite(LED2, HIGH); */
-  /* // crea topics usando id de la placa */
-  strcpy(ID_PLACA,"ESP1");
-  sprintf(topic_SUB, "II3/ESP1/conexion");
-  conecta_wifi();
-  
-  mqtt_client.setServer(mqtt_server, 1883);
-  mqtt_client.setBufferSize(512); // para poder enviar mensajes de hasta X bytes
-  mqtt_client.setCallback(procesa_mensaje);
-  conecta_mqtt();
+  /* setup de infrastructura */
+  objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CFG] = strTopicCfg;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
+  objInfra.objConfig["CRONO_INI"] ="30:00";
+  objInfra.objConfig["CRONO_MAX"] ="90:00";
+  objInfra.objConfig["CRONO_ADD"] ="10:00";
+  objInfra.objConfig["FREQ_START"]=1200;
+  objInfra.objConfig["FREQ_CARA"] =1300;
+  objInfra.objConfig["FREQ_FIN"]  =1400;
+  objInfra.objConfig["DURA_START"]=5000;
+  objInfra.objConfig["DURA_CARA"] =2000;
+  objInfra.objConfig["DURA_FIN"]  =10000;
 
-  Serial.printf("Identificador placa: %s\n", ID_PLACA );
-  Serial.printf("Topic publicacion  : %s\n", topic_PUB);
-  Serial.printf("Topic subscripcion : %s\n", topic_SUB);
-  Serial.printf("Termina setup en %lu ms\n\n",millis());
-
+  objInfra.Setup(mqttCallback);
 }
 
-//-----------------------------------------------------
 
-//-----------------------------------------------------
+/* Codificacion de estados */
+/* STATES */
+/* Waiting : 0 */
+/* InGame : 1 */
+/* Finished : 2 */
 
-//     LOOP
-//-----------------------------------------------------
 
+void SendState(){
+  char *msg = "{\"state\": \"\"}";
+  switch(state) 
+    {
+    case 0:
+      msg = "{\"state\": \"waiting\"}";
+      break;
+    case 1:
+      msg = "{\"state\": \"ingame\"}";
+      break;
+    case 2:
+      msg = "{\"state\": \"finished\"}";      
+      break;
+    default:
+      Serial.println("States failed!");
+    }
+  
+  // objInfra.MqttPublish(msg);
+}
 
+int count_digit(int number) {
+  return int(log10(number) + 1);
+}
+
+#define MESSAGE_SIZE_ 300
 
 void loop() {
-  static int ans_user = 0;
-   if (!mqtt_client.connected()) conecta_mqtt();
-  mqtt_client.loop(); // esta llamada para que la librería recupere el control
+  StaticJsonDocument<MESSAGE_SIZE_> json;
+  static char message[MESSAGE_SIZE_];
   
- /* GetData(); */
- /* ans_user = GetData(); */
-    if (ans_user == ans){
-     Serial.printf("You win!");
-  }
+  static int digits = 0;
+  static int number = 0;
+  static int intentos = 0;
+  char *msg;
+  
+  objInfra.Loop();
+
+  if(state == 0){/* Waiting */
+    if(game_ans != NULL){
+      digits = count_digit(game_ans);
+      state = 1;
+    }
+  }else if(state == 1){/* InGame */
+
+    char key = myKeypad.getKey();
     
-  char myKey = myKeypad.getKey();
-  
-  if (myKey != NULL){
-  Serial.print("Key pressed: ");
-  Serial.println(myKey);
-  Serial.print("\n");
+    if (key != NULL && key >= 48 && key <= 59){
+      
+      number = 10*number + (key-48);
+      if(count_digit(number) == digits){
+        Serial.println(number);
+
+        if (game_ans == number){
+          
+          Serial.println("You win!");
+          state = 2;
+          
+        }else{
+          intentos++;
+          number = 0;
+        }        
+      }
+    }
+  }else if(state == 2){/* Finishes */
+    json["state"] = "finished";
+    json["intentos"] = intentos+1;
+    json["tiempo"] = "3600";
+
+    serializeJson(json,message);
+    objInfra.MqttPublish(message);
+    Serial.println("El juego ha terminado!");
+    state = 0;
+  }else{
+    Serial.println("States failed!");
   }
-  //Serial.println(ans);
-  //Serial.println(ans_user);
+
+  SendState();
 }
+
+
+/* Bugs*/
+/* Si con dos dígitos intento escribir 01 o 02 no funciona porque */
+/* la función count calcula el log10 y no va bien por ahora me funciona */
+/* pero tendré que cambiarlo en el futuro */
+/* int count_digit(int number) { */
+/*    int count = 0; */
+/*    while(number != 0) { */
+/*       number = number / 10; */
+/*       count++; */
+/*    } */
+/*    return count; */
+
+
+/* Features */
+/* Activar una pista clásica en un texto del dashboard + o - caliente o frío*/
+/* Explicar un poco el acertijo, lo cual tiene más trabajo por parte del desarrollador */
+
+
