241f75fe29abfad0a1d85ab708d63498d3ed5879
izanamador <izan.amador@gmail.com>
2023-01-05 12:44:36 +0100

    corrected cara006

--- la-caja/arduino/cara006/cara006.ino
+++ la-caja/arduino/cara006/cara006.ino
@@ -1,78 +1,135 @@
  /*******************************************************
-Enter the next value of a discrete equation sequence of an 
-IQ test in binary by means of switches and a button.
-The possible number to represent will be 2^n-1, 
-thus having for n = 5 switches, the representation 
-of the range [0-31].
-The button will be used to send the correct answer.
+The player has to send the message SOS with morse code. 
+If the LDR is light up close by a phone light will send a dot.
+If not, it will send a dash.
 
-This version is only an example with the following problem:
-Secuence: 3 5 7 11 13 
-Correct number: 17
+If the player clicks the button it will add the letter of the morse code to the message
+To reset the message the player must pulse the button for a longer time. 
+
+SOS is ...---...
 ********************************************************/
 // Include Libraries
 #include "Arduino.h"
 #include "Button.h"
 
 // Pin Definitions
+#define LDR_PIN A0 // Pin for the LDR sensor
 #define PUSHBUTTON_PIN_2  15
-#define TOGGLESWITCH_1_PIN_2  4
-#define TOGGLESWITCH_2_PIN_2  10
-#define TOGGLESWITCH_3_PIN_2  2
-#define TOGGLESWITCH_4_PIN_2  14
-#define TOGGLESWITCH_5_PIN_2  12
 
-// Global variables and defines
+// Millis timing
+unsigned long buttonPressTime = 0; // Store the time when the button was last pressed
+unsigned long previousMillis = 0; // Store the previous time in milliseconds
+const long buttonClearTime = 5000; // Threshold time for clearing the message string, in milliseconds
+long interval = 1000; // Interval between readings in milliseconds
 
-// object initialization
+// Object inicialization
 Button pushButton(PUSHBUTTON_PIN_2);
-Button ToggleSwitch_1(TOGGLESWITCH_1_PIN_2);
-Button ToggleSwitch_2(TOGGLESWITCH_2_PIN_2);
-Button ToggleSwitch_3(TOGGLESWITCH_3_PIN_2);
-Button ToggleSwitch_4(TOGGLESWITCH_4_PIN_2);
-Button ToggleSwitch_5(TOGGLESWITCH_5_PIN_2);
 
-//define vars for program
-int COMBINACION_CORRECTA_001 = 17;
+// String Inicialization
+String morseCode = ""; // String to store the Morse code message
+String message = ""; // String to store the translated message
+String GameAns = "SOS";
 
-void setup() 
-{
-    Serial.begin(9600);
-    while (!Serial) ; // wait for serial port to connect. Needed for native USB
-    Serial.println("start");
-   
-    pushButton.init();
-    ToggleSwitch_1.init();
-    ToggleSwitch_2.init();
-    ToggleSwitch_3.init();
-    ToggleSwitch_4.init();
-    ToggleSwitch_5.init();
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  pinMode(LDR_PIN, INPUT); // Set the LDR pin as an input
+  pushButton.init();
 }
 
-void loop() 
-{
-  std::array<int, 5> COMBINACION_ACTUAL_ARRAY {
-    ToggleSwitch_1.read(),
-    ToggleSwitch_2.read(),
-    ToggleSwitch_3.read(),
-    ToggleSwitch_4.read(),
-    ToggleSwitch_5.read()};
-
-   bool pushButtonVal = pushButton.read();
-
-  int COMBINACION_ACTUAL = 0;
+void loop() {
   
-  for (int i = 0; i < 5; i++) {
-    COMBINACION_ACTUAL += COMBINACION_ACTUAL_ARRAY[i] << i;
+bool pushButtonVal = pushButton.read(); // Read the button value
+
+// Clears the message after a long time
+if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime) {
+  message = ""; // Clear the message string
+  buttonPressTime = millis(); // Update the button press time
+}
+
+// Update the button press time when the button is pressed
+if (pushButtonVal == LOW) {
+  buttonPressTime = millis();
+}
+
+  int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor 
+  //Serial.println(ldrValue); // CALIBRATION
+
+   unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
+  
+    if (ldrValue < 100) { // If the LDR value is above a certain threshold, it means the light is on
+      morseCode += "."; // Add a "dot" to the Morse code message
+      interval = 3000; // Set the interval to 1 second for the next reading
+    } else {
+      morseCode += "-"; // Add a "dash" to the Morse code message
+      interval = 6000; // Set the interval to 3 seconds for the next reading
+    }
   }
-
-
-  if(pushButtonVal == HIGH)
-  {
-      Serial.println(COMBINACION_ACTUAL);
- if(COMBINACION_ACTUAL == COMBINACION_CORRECTA_001)
-      { 
-      Serial.println(F("¡Ganaste! Felicidades Tienes 150 de IQ"));
+  
+  Serial.println(morseCode); // CALIBRATION
+    
+  // Check if a complete Morse code character has been received
+  if (pushButtonVal == HIGH) { // A space indicates the end of a character
+    char c = decodeMorse(morseCode); // Decode the character
+    if (c != '\0') { // If the character is valid, add it to the message
+      message += c;
+      if (GameAns == message)
+      {
+        Serial.println("YOU WON");
       }
+    }
+    morseCode = ""; // Clear the Morse code string for the next character
   }
+  Serial.println(message); // Print the message to the serial monitor
+}
+
+// Function to decode a Morse code character
+char decodeMorse(String code) {
+  // Morse code lookup table
+  const char morseTable[26][5] = {
+    ".-",   // A
+    "-...", // B
+    "-.-.", // C
+    "-..",  // D
+    ".",    // E
+    "..-.", // F
+    "--.",  // G
+    "....", // H
+    "..",   // I
+    ".---", // J
+    "-.-",  // K
+    ".-..", // L
+    "--",   // M
+    "-.",   // N
+    "---",  // O
+    ".--.", // P
+    "--.-", // Q
+    ".-.",  // R
+    "...",  // S
+    "-",    // T
+    "..-",  // U
+    "...-", // V
+    ".--",  // W
+    "-..-", // X
+    "-.--", // Y
+    "--.."  // Z
+  };
+  
+  // Check if the code is a space
+  if (code == " ") {
+    return ' ';
+  }
+  
+  // Search for the code in the lookup table
+  for (int i = 0; i < 26; i++) {
+    if (code == morseTable[i]) {
+      return (char)('A' + i); // Return the corresponding letter
+    }
+  }
+  
+  // If the code is not found in the lookup table, return a null character
+  return '\0';
 }
