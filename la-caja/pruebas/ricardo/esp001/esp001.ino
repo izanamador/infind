b7279cabf356ae53fafb306ad7aa5720565afc80
rpluma <99877837+rpluma@users.noreply.github.com>
2023-01-10 08:50:44 +0100

    Estado repo prev inicio cambios masivos

--- la-caja/pruebas/ricardo/esp001/esp001.ino
+++ la-caja/pruebas/ricardo/esp001/esp001.ino
@@ -1,14 +1,14 @@
- #include "EspInfInd.h"
+ //#include "EspInfInd.h"
  EspInfInd objEsp;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  delay(1000);
-  objEsp.setup();
-  delay(2000);
+    pinMode(LED_BUILTIN, OUTPUT);
+    delay(1000);
+    objEsp.setup();
+    delay(2000);
 }
 
 // the loop function runs over and over again forever
