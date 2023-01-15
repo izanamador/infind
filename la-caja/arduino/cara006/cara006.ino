e69d92c59200673bb855770fe827973899489435
izanamador <izan.amador@gmail.com>
2023-01-15 02:29:12 +0100

    updates in cara006 cara005

--- la-caja/arduino/cara006/cara006.ino
+++ la-caja/arduino/cara006/cara006.ino
@@ -1,84 +1,141 @@
- /*******************************************************
-The player has to send the message SOS with morse code. 
+/*******************************************************
+The player has to send the message SOS with morse code.
 If the LDR is light up close by a phone light will send a dot.
 If not, it will send a dash.
 
 If the player clicks the button it will add the letter of the morse code to the message
-To reset the message the player must pulse the button for a longer time. 
+To reset the message the player must pulse the button for a longer time.
 
 SOS is ...---...
 ********************************************************/
+
+/****************************/
+/* Includes and definitions */
+/****************************/
+
 // Include Libraries
-#include "Arduino.h"
+#include <infra.h>
+#include <ArduinoJson.h>
 #include "Button.h"
+
+// Infra setup
+Infra objInfra;
+char *strTopicPub = "II3/ESP006/pub/cara006"; // topic principal para publicar contenido y lastwill
+char *strTopicCfg = "II3/ESP006/cfg/cara006"; // topic para recibir parametros de configuracion
+char *strTopicCmd = "II3/ESP006/cmd/cara006"; // topic para recibir peticiones de comando
 
 // Pin Definitions
 #define LDR_PIN A0 // Pin for the LDR sensor
-#define PUSHBUTTON_PIN_2  15
+#define PUSHBUTTON_PIN_2 15
 
 // Millis timing
 unsigned long buttonPressTime = 0; // Store the time when the button was last pressed
-unsigned long previousMillis = 0; // Store the previous time in milliseconds
+unsigned long previousMillis = 0;  // Store the previous time in milliseconds
 const long buttonClearTime = 5000; // Threshold time for clearing the message string, in milliseconds
-long interval = 1000; // Interval between readings in milliseconds
+long interval = 1000;              // Interval between readings in milliseconds
 
 // Object inicialization
 Button pushButton(PUSHBUTTON_PIN_2);
 
 // String Inicialization
 String morseCode = ""; // String to store the Morse code message
-String message = ""; // String to store the translated message
+String message = "   ";   // String to store the translated message
 String GameAns = "SOS";
 
-void setup() {
-  Serial.begin(9600); // Initialize serial communication
+
+/****************************/
+/*          Game            */
+/****************************/
+void setup()
+{
   pinMode(LDR_PIN, INPUT); // Set the LDR pin as an input
   pushButton.init();
+  /* setup de infrastructura */
+  objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CFG] = strTopicCfg;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
+  objInfra.Setup(mqttCallback);
 }
 
-void loop() {
-  
-bool pushButtonVal = pushButton.read(); // Read the button value
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  char *mensaje = (char *)malloc(length + 1);
+  strncpy(mensaje, (char *)payload, length);
+  mensaje[length] = '\0';
+  Serial.printf("Mensaje recibido [%s] %s\n", topic, mensaje);
 
-// Clears the message after a long time
-if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime) {
-  message = ""; // Clear the message string
-  buttonPressTime = millis(); // Update the button press time
+  if (strcmp(topic, strTopicCmd) == 0)
+  {
+    //GameAns = atoi(mensaje);
+    objInfra.ReportStart(NULL);
+  }
+  //Serial.println(GameAns);
+  free(mensaje);
 }
+ static char strDigits[10]= "";
+void loop()
+{
+ 
+  objInfra.Loop();
+  if (!objInfra.GameRunning())
+    return;
 
-// Update the button press time when the button is pressed
-if (pushButtonVal == LOW) {
-  buttonPressTime = millis();
-}
+  bool pushButtonVal = pushButton.read(); // Read the button value
 
-  int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor 
-  //Serial.println(ldrValue); // CALIBRATION
+  // Clears the message after a long time
+  if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime)
+  {
+    message = "";               // Clear the message string
+    strcpy(strDigits,"");        /* Limpio el string en Node-Red */
+    buttonPressTime = millis(); // Update the button press time
+    //objInfra.ReportFailure(" ");  // Cast the const char* to a char*
+  }
 
-   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  // Update the button press time when the button is pressed
+  if (pushButtonVal == LOW)
+  {
+    buttonPressTime = millis();
+  }
+
+  int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
+                                      // Serial.println(ldrValue); // CALIBRATION
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
-    
+
     int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
-  
-    if (ldrValue < 100) { // If the LDR value is above a certain threshold, it means the light is on
+
+    if (ldrValue < 100)
+    {                   // If the LDR value is above a certain threshold, it means the light is on
       morseCode += "."; // Add a "dot" to the Morse code message
-      interval = 3000; // Set the interval to 1 second for the next reading
-    } else {
+      interval = 3000;  // Set the interval to 1 second for the next reading
+    }
+    else
+    {
       morseCode += "-"; // Add a "dash" to the Morse code message
-      interval = 6000; // Set the interval to 3 seconds for the next reading
+      interval = 6000;  // Set the interval to 3 seconds for the next reading
     }
   }
-  
+Serial.println(pushButtonVal);
   Serial.println(morseCode); // CALIBRATION
-    
+
+
   // Check if a complete Morse code character has been received
-  if (pushButtonVal == HIGH) { // A space indicates the end of a character
+  if (pushButtonVal == HIGH)
+  {                                  // A space indicates the end of a character
     char c = decodeMorse(morseCode); // Decode the character
-    if (c != '\0') { // If the character is valid, add it to the message
+    //objInfra.ReportStatus(c);
+    if (c != '\0')
+    { // If the character is valid, add it to the message
       message += c;
+       message.toCharArray(strDigits, 10);
+    ///  objInfra.ReportStatus(strDigits);
       if (GameAns == message)
       {
         Serial.println("YOU WON");
+        objInfra.ReportSuccess(" ");  // Cast the const char* to a char*
       }
     }
     morseCode = ""; // Clear the Morse code string for the next character
@@ -86,50 +143,60 @@ if (pushButtonVal == LOW) {
   Serial.println(message); // Print the message to the serial monitor
 }
 
+
+
+/****************************/
+/*    Aditional Functions   */
+/****************************/
+
 // Function to decode a Morse code character
-char decodeMorse(String code) {
+char decodeMorse(String code)
+{
   // Morse code lookup table
   const char morseTable[26][5] = {
-    ".-",   // A
-    "-...", // B
-    "-.-.", // C
-    "-..",  // D
-    ".",    // E
-    "..-.", // F
-    "--.",  // G
-    "....", // H
-    "..",   // I
-    ".---", // J
-    "-.-",  // K
-    ".-..", // L
-    "--",   // M
-    "-.",   // N
-    "---",  // O
-    ".--.", // P
-    "--.-", // Q
-    ".-.",  // R
-    "...",  // S
-    "-",    // T
-    "..-",  // U
-    "...-", // V
-    ".--",  // W
-    "-..-", // X
-    "-.--", // Y
-    "--.."  // Z
+      ".-",   // A
+      "-...", // B
+      "-.-.", // C
+      "-..",  // D
+      ".",    // E
+      "..-.", // F
+      "--.",  // G
+      "....", // H
+      "..",   // I
+      ".---", // J
+      "-.-",  // K
+      ".-..", // L
+      "--",   // M
+      "-.",   // N
+      "---",  // O
+      ".--.", // P
+      "--.-", // Q
+      ".-.",  // R
+      "...",  // S
+      "-",    // T
+      "..-",  // U
+      "...-", // V
+      ".--",  // W
+      "-..-", // X
+      "-.--", // Y
+      "--.."  // Z
   };
-  
+
   // Check if the code is a space
-  if (code == " ") {
+  if (code == " ")
+  {
     return ' ';
   }
-  
+
   // Search for the code in the lookup table
-  for (int i = 0; i < 26; i++) {
-    if (code == morseTable[i]) {
+  for (int i = 0; i < 26; i++)
+  {
+    if (code == morseTable[i])
+    {
       return (char)('A' + i); // Return the corresponding letter
     }
   }
-  
+
   // If the code is not found in the lookup table, return a null character
   return '\0';
 }
--- la-caja/pruebas/izan/barco/barco.ino
+++ la-caja/pruebas/izan/barco/barco.ino
@@ -1,74 +1,90 @@
+/*******************************************************
+The player has to send the message SOS with morse code.
+If the LDR is light up close by a phone light will send a dot.
+If not, it will send a dash.
+
+If the player clicks the button it will add the letter of the morse code to the message
+To reset the message the player must pulse the button for a longer time.
+
+*! SOS is ...---... 
+********************************************************/
 // Include Libraries
 #include "Arduino.h"
 #include "Button.h"
 
 // Pin Definitions
 #define LDR_PIN A0 // Pin for the LDR sensor
-#define PUSHBUTTON_PIN_2  15
+#define PUSHBUTTON_PIN_2 15
 
+// Millis timing
 unsigned long buttonPressTime = 0; // Store the time when the button was last pressed
+unsigned long previousMillis = 0;  // Store the previous time in milliseconds
 const long buttonClearTime = 5000; // Threshold time for clearing the message string, in milliseconds
+long interval = 1000;              // Interval between readings in milliseconds
 
-
+// Object inicialization
 Button pushButton(PUSHBUTTON_PIN_2);
 
+// String Inicialization
 String morseCode = ""; // String to store the Morse code message
-String message = ""; // String to store the translated message
+String message = "";   // String to store the translated message
 String GameAns = "SOS";
 
-unsigned long previousMillis = 0; // Store the previous time in milliseconds
-long interval = 1000; // Interval between readings in milliseconds
-
-
-void setup() {
-  Serial.begin(9600); // Initialize serial communication
+void setup()
+{
+  Serial.begin(9600);      // Initialize serial communication
   pinMode(LDR_PIN, INPUT); // Set the LDR pin as an input
   pushButton.init();
 }
 
-void loop() {
-  
-   // Read the button state
-   
-bool pushButtonVal = pushButton.read(); // Read the button value
+void loop()
+{
 
-// Check if the button has been pressed for a certain amount of time
-if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime) {
-  message = ""; // Clear the message string
-  buttonPressTime = millis(); // Update the button press time
-}
+  bool pushButtonVal = pushButton.read(); // Read the button value
 
-// Update the button press time when the button is pressed
-if (pushButtonVal == LOW) {
-  buttonPressTime = millis();
-}
+  // Clears the message after a long time
+  if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime)
+  {
+    message = "";               // Clear the message string
+    buttonPressTime = millis(); // Update the button press time
+  }
+
+  // Update the button press time when the button is pressed
+  if (pushButtonVal == LOW)
+  {
+    buttonPressTime = millis();
+  }
 
   int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
-  
-  
-  Serial.println(ldrValue); // CALIBRATION
+                                      // Serial.println(ldrValue); // CALIBRATION
 
-   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
-    
+
     int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
-  
-    if (ldrValue < 100) { // If the LDR value is above a certain threshold, it means the light is on
+
+    if (ldrValue < 100)
+    {                   // If the LDR value is above a certain threshold, it means the light is on
       morseCode += "."; // Add a "dot" to the Morse code message
-      interval = 3000; // Set the interval to 1 second for the next reading
-    } else {
+      interval = 3000;  // Set the interval to 1 second for the next reading
+    }
+    else
+    {
       morseCode += "-"; // Add a "dash" to the Morse code message
-      interval = 6000; // Set the interval to 3 seconds for the next reading
+      interval = 6000;  // Set the interval to 3 seconds for the next reading
     }
   }
-  
+
   Serial.println(morseCode); // CALIBRATION
-    
+
   // Check if a complete Morse code character has been received
-  if (pushButtonVal == HIGH) { // A space indicates the end of a character
+  if (pushButtonVal == HIGH)
+  {                                  // A space indicates the end of a character
     char c = decodeMorse(morseCode); // Decode the character
-    if (c != '\0') { // If the character is valid, add it to the message
+    if (c != '\0')
+    { // If the character is valid, add it to the message
       message += c;
       if (GameAns == message)
       {
@@ -77,57 +93,57 @@ if (pushButtonVal == LOW) {
     }
     morseCode = ""; // Clear the Morse code string for the next character
   }
-
   Serial.println(message); // Print the message to the serial monitor
 }
 
-
-
-
 // Function to decode a Morse code character
-char decodeMorse(String code) {
+char decodeMorse(String code)
+{
   // Morse code lookup table
   const char morseTable[26][5] = {
-    ".-",   // A
-    "-...", // B
-    "-.-.", // C
-    "-..",  // D
-    ".",    // E
-    "..-.", // F
-    "--.",  // G
-    "....", // H
-    "..",   // I
-    ".---", // J
-    "-.-",  // K
-    ".-..", // L
-    "--",   // M
-    "-.",   // N
-    "---",  // O
-    ".--.", // P
-    "--.-", // Q
-    ".-.",  // R
-    "...",  // S
-    "-",    // T
-    "..-",  // U
-    "...-", // V
-    ".--",  // W
-    "-..-", // X
-    "-.--", // Y
-    "--.."  // Z
+      ".-",   // A
+      "-...", // B
+      "-.-.", // C
+      "-..",  // D
+      ".",    // E
+      "..-.", // F
+      "--.",  // G
+      "....", // H
+      "..",   // I
+      ".---", // J
+      "-.-",  // K
+      ".-..", // L
+      "--",   // M
+      "-.",   // N
+      "---",  // O
+      ".--.", // P
+      "--.-", // Q
+      ".-.",  // R
+      "...",  // S
+      "-",    // T
+      "..-",  // U
+      "...-", // V
+      ".--",  // W
+      "-..-", // X
+      "-.--", // Y
+      "--.."  // Z
   };
-  
+
   // Check if the code is a space
-  if (code == " ") {
+  if (code == " ")
+  {
     return ' ';
   }
-  
+
   // Search for the code in the lookup table
-  for (int i = 0; i < 26; i++) {
-    if (code == morseTable[i]) {
+  for (int i = 0; i < 26; i++)
+  {
+    if (code == morseTable[i])
+    {
       return (char)('A' + i); // Return the corresponding letter
     }
   }
-  
+
   // If the code is not found in the lookup table, return a null character
   return '\0';
 }
