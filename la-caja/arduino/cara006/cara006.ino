b83240146866c1155f9c40734e7770682bf6a5bd
izanamador <izan.amador@gmail.com>
2023-01-22 19:16:19 +0100

    debug rc=-2 mqtt

--- la-caja/arduino/cara006/cara006.ino
+++ la-caja/arduino/cara006/cara006.ino
@@ -1,3 +1,220 @@
+void decodeMorse(char* strSymbols, char *strPhrase)
+{
+  // Morse code lookup table
+  const char morseTable[26][6] = {
+      ".- ",   // A
+      "-... ", // B
+      "-.-. ", // C
+      "-.. ",  // D
+      ". ",    // E
+      "..-. ", // F
+      "--. ",  // G
+      ".... ", // H
+      ".. ",   // I
+      ".--- ", // J
+      "-.- ",  // K
+      ".-.. ", // L
+      "-- ",   // M
+      "-. ",   // N
+      "--- ",  // O
+      ".--. ", // P
+      "--.- ", // Q
+      ".-. ",  // R
+      "... ",  // S
+      "- ",    // T
+      "..- ",  // U
+      "...- ", // V
+      ".-- ",  // W
+      "-..- ", // X
+      "-.-- ", // Y
+      "--.. "  // Z
+  };
+
+  int iPositionIn = 0;
+  int iPositionOut = 0;
+  int j;
+  char strSymbolsEsp [100];
+  sprintf(strSymbolsEsp, "%s ", strSymbols);
+  Serial.printf("--------->>>>>>>>>> Decodificando %s\n", strSymbols);
+  strcpy(strSymbols, strSymbolsEsp);
+
+  while (true) {
+    Serial.printf("Buscando en %s\n", (char*)(strSymbols+iPositionIn));
+    if (strSymbols[iPositionIn]=='\0') break;
+    if (strSymbols[iPositionIn]==' ') {iPositionIn++;continue;}
+    for (j=0; j<26; j++) {
+      if (strncmp(strSymbols+iPositionIn, morseTable[j], strlen(morseTable[j]))==0) {
+        Serial.printf("%s=%c\t", morseTable[j], (char)('A'+j));
+        strPhrase[iPositionOut++] = (char)('A'+j);
+        iPositionIn += strlen(morseTable[j]);
+        break;
+      }
+    }
+    if (j>=26)  {
+      strPhrase[iPositionOut++] = '?';
+      
+      while ((strSymbols[iPositionIn] != ' ') && (strSymbols[iPositionIn] != '\0')) {
+        Serial.printf("%c", strSymbols[iPositionIn]);
+        iPositionIn++;
+      }
+      Serial.printf("=?\t");        
+    }
+
+    if (strSymbols[iPositionIn] == '\0') {
+      strPhrase[iPositionOut] = '\0';
+      Serial.printf("\n---------<<<<<<<<<<[%s] = [%s]\n", strSymbols, strPhrase);
+      return;
+    }
+    else if (strSymbols[iPositionIn] ==' ')
+      iPositionIn++;
+  }
+}
+
+
+//--------------------------------------------------------------------------- TM1637
+  #ifndef IIFLASH_
+  #define IIFLASH_
+
+  #define D3_GPIO00_FLASH     0
+  #define IIBUTTON_MODE_BINARY 1
+  #define IIBUTTON_MODE_ANALOG 2
+
+  #define IIBUTTON_INTV_INIT    'I'  // inicialización
+  #define IIBUTTON_INTV_SIGNAL  's'  // sin actividad (desde últ bajada a subida) entre 100 y 500 ms
+  #define IIBUTTON_INTV_LETTER  'L'  // sin actividad entre 501 y 1000 ms
+  #define IIBUTTON_INTV_WORD    'W'  // sin actividad por más de 1000 ms
+  
+  #define IIBUTTON_SIGN_INIT    'I'     // activo entre 100 y 500 ms
+  #define IIBUTTON_SIGN_DOT     '.'     // activo entre 100 y 500 ms
+  #define IIBUTTON_SIGN_DASH    '-'    // activo entre 501 y 1000 ms
+  #define IIBUTTON_SIGN_LONG    '_'    // activo por más de 1000 ms
+
+  class IIButton {
+    private:
+      int pinnum_; // pin al que está conectado el botón
+      int pinmod_; // modo del botón (binario o analógico)
+      int thresh_             = 128; // threshold para HIGH/LOW en caso de entrada analógica
+
+      unsigned int ms00s0_      = 0;  // tiempo del flanco anterior de bajada (letra a)
+      unsigned int ms01s1_      = 0;  // tiempo del flanco de subida en el signo anterior
+      unsigned int ms10s1_      = 0;  // tiempo del flanco de bajada en el signo anterior
+      unsigned int ms01s2_      = 0;  // tiempo del flanco de subida en el signo actual
+      unsigned int ms10s2_      = 0;  // tiempo del flanco de bajada en el signo actual
+      unsigned int prevdurs_    = 0;  // duración de la señal anterior
+      unsigned int currdurs_    = 0;  // duración de la señal actual
+      unsigned int prevduri_    = 0;  // duración del intervalo anterior
+      unsigned int currduri_    = 0;  // duración del intervalo actual
+    
+      int currvalu_ = HIGH;
+      int nextvalu_ = HIGH;
+      unsigned int msTransit  = 0;
+      int iSymbol_, iPhrase_;
+
+      void getNextValu() {
+        if (pinmod_ == IIBUTTON_MODE_BINARY) {
+          nextvalu_ = digitalRead(pinnum_);
+        } else {
+          nextvalu_ = analogRead(pinnum_);
+          nextvalu_ = (nextvalu_ < thresh_) ? LOW : HIGH;
+        }
+      }
+  
+    public:
+      char prevsign             = IIBUTTON_SIGN_INIT; // clasificación de la duración del último intervalo I=inicial, s=signo, L=letter, W=word
+      char currsign             = IIBUTTON_SIGN_INIT; // ídem del signo actual
+      char previntv             = IIBUTTON_INTV_INIT; // clasificación de la duración del último intervalo I=inicial, s=signo, L=letter, W=word
+      char currintv             = IIBUTTON_INTV_INIT; // ídem del signo actual
+      char strSymbols[40]; // W...L---L...W
+      char strPhrase[10];  // SOS SE ACABO
+
+      
+      // inicializa el botón y el modo digital o analógico;
+        void Setup(int pinNum=D3_GPIO00_FLASH, int pinMod = IIBUTTON_MODE_BINARY) { 
+          //--- Inicializaciones específicas del actuador
+            pinnum_ = pinNum;
+            pinmod_ = pinMod;
+            getNextValu();
+            currvalu_ = nextvalu_;      
+            iSymbol_ = 0;
+            iPhrase_ = 0;
+            strSymbols[iSymbol_] = '\0';
+            strPhrase[iPhrase_] = '\0';
+        }         
+      // Setup
+      void Reset() {
+        iSymbol_ = 0;
+            iPhrase_ = 0;
+            strSymbols[iSymbol_] = '\0';
+            strPhrase[iPhrase_] = '\0';                
+      }
+      
+      // Comprueba si ha habido un flanco de subida o bajada
+        bool HasChanged(void) {         
+          unsigned int now = millis();
+          getNextValu();
+                      
+          if (currvalu_ == nextvalu_) // aún no ha pasado nada, esperamos a una transición
+            msTransit = now;
+          else if (now - msTransit < 30) // aún no han pasado 100 milisegundos en el nuevo estado, esperar
+            ;
+          else { // se confirma la transición después de 100 ms
+            if (currvalu_ == HIGH && nextvalu_== LOW) { // flanco positivo, se ha pulsado el botón (REVISAR)
+              currvalu_ = LOW;
+              ms01s1_ = ms01s2_;
+              ms01s2_ = msTransit;              
+            }
+            else if (currvalu_ == LOW && nextvalu_==HIGH){
+              currvalu_  = HIGH;
+              ms00s0_    = ms10s1_;
+              ms10s1_    = ms10s2_;
+              ms10s2_    = msTransit;
+              // shift de valores curr a valores prev
+              prevsign   = currsign;
+              previntv   = currintv;
+              prevdurs_  = currdurs_;
+              prevduri_  = currduri_;
+              // cálculo de valores current              
+              currdurs_  = ms10s2_ - ms01s2_; // flanco bajada s2 - flanco subida s2
+              currduri_  = ms01s2_ - ms10s1_; // flanco subida s2 - flanco bajada s1
+              currsign   = (currdurs_ < 300) ? IIBUTTON_SIGN_DOT    : ((currdurs_ < 1500) ? IIBUTTON_SIGN_DASH   : IIBUTTON_SIGN_LONG );              
+              currintv   = (currduri_ < 300) ? IIBUTTON_INTV_SIGNAL : ((currduri_ < 1500) ? IIBUTTON_INTV_LETTER : IIBUTTON_INTV_WORD );
+              Serial.printf("%c%c ", currintv, currsign);
+              if (currintv==IIBUTTON_INTV_WORD) { // poner un espacio antes si estamos empezando una palabra
+                strSymbols[iSymbol_++] = ' ';
+                strSymbols[iSymbol_+1] = '\0';
+              }
+              if (currsign==IIBUTTON_SIGN_DOT) {
+                strSymbols[iSymbol_++] = '.';
+                strSymbols[iSymbol_+1] = '\0';
+              }                
+              else if (currsign==IIBUTTON_SIGN_DASH) {
+                strSymbols[iSymbol_++] = '-';
+                strSymbols[iSymbol_+1] = '\0';
+              }
+              Serial.println(strSymbols);
+              decodeMorse(strSymbols, strPhrase);
+              Serial.println(strPhrase);
+              
+              
+              return true;
+            }
+          }
+          return false;
+        }         
+       // HasChanged
+       
+
+
+  
+  };
+  IIButton objFlash; 
+
+  #endif  
+
+//--------------------------------------------------------------------------- TM1637
+
+
+
 /*******************************************************
 The player has to send the message SOS with morse code.
 If the LDR is light up close by a phone light will send a dot.
@@ -15,32 +232,17 @@ SOS is ...---...
 
 // Include Libraries
 #include <infra.h>
-#include <ArduinoJson.h>
-#include "Button.h"
 
 // Infra setup
 Infra objInfra;
 char *strTopicPub = "II3/ESP006/pub/cara006"; // topic principal para publicar contenido y lastwill
-//char *strTopicCfg = "II3/ESP006/cfg/cara006"; // topic para recibir parametros de configuracion
 char *strTopicCmd = "II3/ESP006/cmd/cara006"; // topic para recibir peticiones de comando
 
 // Pin Definitions
 #define LDR_PIN A0 // Pin for the LDR sensor
-#define PUSHBUTTON_PIN_2 15
-
-// Millis timing
-unsigned long buttonPressTime = 0; // Store the time when the button was last pressed
-unsigned long previousMillis = 0;  // Store the previous time in milliseconds
-const long buttonClearTime = 5000; // Threshold time for clearing the message string, in milliseconds
-long interval = 1000;              // Interval between readings in milliseconds
-
-// Object inicialization
-Button pushButton(PUSHBUTTON_PIN_2);
 
 // String Inicialization
-String morseCode = ""; // String to store the Morse code message
-String message = "   ";   // String to store the translated message
-String GameAns = "SOS";
+char* GameAns = "SOS";
 
 
 /****************************/
@@ -48,11 +250,9 @@ String GameAns = "SOS";
 /****************************/
 void setup()
 {
-  pinMode(LDR_PIN, INPUT); // Set the LDR pin as an input
-  pushButton.init();
+  objFlash.Setup(A0,IIBUTTON_MODE_ANALOG);     
   /* setup de infrastructura */
   objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
-//  objInfra.mqttTopicsSub[TOPIC_NUM_CFG] = strTopicCfg;
   objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
   objInfra.Setup(mqttCallback);
 }
@@ -66,13 +266,12 @@ void mqttCallback(char *topic, byte *payload, unsigned int length)
 
   if (strcmp(topic, strTopicCmd) == 0)
   {
-    //GameAns = atoi(mensaje);
     objInfra.ReportStart(NULL);
   }
-  //Serial.println(GameAns);
   free(mensaje);
 }
  static char strDigits[10]= "";
+ 
 void loop()
 {
  
@@ -80,123 +279,17 @@ void loop()
   if (!objInfra.GameRunning())
     return;
 
-  bool pushButtonVal = pushButton.read(); // Read the button value
-
-  // Clears the message after a long time
-  if (pushButtonVal == LOW && millis() - buttonPressTime >= buttonClearTime)
-  {
-    message = "";               // Clear the message string
-    strcpy(strDigits,"");        /* Limpio el string en Node-Red */
-    buttonPressTime = millis(); // Update the button press time
-    //objInfra.ReportFailure(" ");  // Cast the const char* to a char*
-  }
-
-  // Update the button press time when the button is pressed
-  if (pushButtonVal == LOW)
-  {
-    buttonPressTime = millis();
-  }
-
-  int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
-                                      // Serial.println(ldrValue); // CALIBRATION
-
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-
-    int ldrValue = analogRead(LDR_PIN); // Read the value of the LDR sensor
-
-    if (ldrValue < 100)
-    {                   // If the LDR value is above a certain threshold, it means the light is on
-      morseCode += "."; // Add a "dot" to the Morse code message
-      interval = 3000;  // Set the interval to 1 second for the next reading
+  bool bchgFlash = objFlash.HasChanged();
+  static unsigned int msLastReport = millis();
+  // comprobamos si han pasado los 15 segundos desde la última vez
+    if ((millis() > msLastReport + 15000) && (bchgFlash)) {
+      msLastReport = millis(); // reseteamos para no enviar más hasta dentro de 15 segundos
     }
-    else
-    {
-      morseCode += "-"; // Add a "dash" to the Morse code message
-      interval = 6000;  // Set the interval to 3 seconds for the next reading
-    }
-  }
-Serial.println(pushButtonVal);
-  Serial.println(morseCode); // CALIBRATION
-
-
-  // Check if a complete Morse code character has been received
-  if (pushButtonVal == HIGH)
-  {                                  // A space indicates the end of a character
-    char c = decodeMorse(morseCode); // Decode the character
-    //objInfra.ReportStatus(c);
-    if (c != '\0')
-    { // If the character is valid, add it to the message
-      message += c;
-       message.toCharArray(strDigits, 10);
-    ///  objInfra.ReportStatus(strDigits);
-      if (GameAns == message)
-      {
-        Serial.println("YOU WON");
-        objInfra.ReportSuccess(" ");  // Cast the const char* to a char*
-      }
-    }
-    morseCode = ""; // Clear the Morse code string for the next character
-  Serial.println(message); // Print the message to the serial monitor
+    
+     // if (GameAns == strPhrase)
+    //  {
+    //    Serial.println("YOU WON");
+    //    objInfra.ReportSuccess(" ");  // Cast the const char* to a char*
+    //  }
 }
-}
-
-
-
-/****************************/
-/*    Aditional Functions   */
-/****************************/
-
-// Function to decode a Morse code character
-char decodeMorse(String code)
-{
-  // Morse code lookup table
-  const char morseTable[26][5] = {
-      ".-",   // A
-      "-...", // B
-      "-.-.", // C
-      "-..",  // D
-      ".",    // E
-      "..-.", // F
-      "--.",  // G
-      "....", // H
-      "..",   // I
-      ".---", // J
-      "-.-",  // K
-      ".-..", // L
-      "--",   // M
-      "-.",   // N
-      "---",  // O
-      ".--.", // P
-      "--.-", // Q
-      ".-.",  // R
-      "...",  // S
-      "-",    // T
-      "..-",  // U
-      "...-", // V
-      ".--",  // W
-      "-..-", // X
-      "-.--", // Y
-      "--.."  // Z
-  };
-
-  // Check if the code is a space
-  if (code == " ")
-  {
-    return ' ';
-  }
-
-  // Search for the code in the lookup table
-  for (int i = 0; i < 26; i++)
-  {
-    if (code == morseTable[i])
-    {
-      return (char)('A' + i); // Return the corresponding letter
-    }
-  }
-
-  // If the code is not found in the lookup table, return a null character
-  return '\0';
-}
+   
