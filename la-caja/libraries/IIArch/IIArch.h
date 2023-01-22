d4ab8d0347e773fd98476570810d5cb91a828d0a
rpluma <99877837+rpluma@users.noreply.github.com>
2023-01-22 08:02:13 +0100

    R45-Mensajes MQTT.3
    Los mensajes de datos contienen los parámetros de la red que hay que enviar por MQTT
    	"Wifi":{"SSId":"infind","IP":"192.168.0.100","RSSI":56}

---
+++ la-caja/libraries/IIArch/IIArch.h
@@ -0,0 +1,16 @@
+/*
+
+IIArch.h
+
+La librería IIArch encapsula la funcionalidad contenida en las especificaciones del proyecto:
+
+- Clase IIWifi: Conexión a la WIFI
+
+*/
+
+#ifndef IIARCH_
+#define IIARCH_
+
+#include "IIWifi.h"
+
+#endif
