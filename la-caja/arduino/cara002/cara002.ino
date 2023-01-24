43556a0fd1934289899c995e285b36b935435ecb
rhyloo <jorge2@uma.es>
2023-01-24 23:35:15 +0100

    Limpieza del código

--- la-caja/arduino/cara002/cara002.ino
+++ la-caja/arduino/cara002/cara002.ino
@@ -1,53 +1,29 @@
-/* Librería general de la caja  */
-#include <infra.h>
-#include <ArduinoJson.h>
-
-
-/* Librerías de uso generales*/
-#include <array>
-#include <string.h>
-#include <Wire.h>
-#include <math.h>
-#include <cstring>
-
-
-/* TODO Estandarizar los nombres de topics en la infrasestructura */
-/* Ejemplo de topic que cumple: #define TOPIC_PUB_ "II3/ESP14440037/resultados_juego2" */
-Infra objInfra;
-char *strTopicPub = "II3/ESP002/pub/cara002"; /* topic principal para publicar contenido y lastwill */
-//char *strTopicCfg = "II3/ESP002/cfg/cara002"; /* topic para recibir parametros de configuracion */
-char *strTopicCmd = "II3/ESP002/cmd/cara002"; /* topic para recibir peticiones de comando */
-
-
+/* --------------------------------------------------------------------------------------- */
+#include <infra.h>  /* Declaración de Infra */
 #include <Keypad.h> /* Libreria para usar el numpad */
 #define NUM_ROWS 4  /* Tamaño del numpad (4 x 4)    */
 #define NUM_COLS 4
-char keys[NUM_ROWS][NUM_COLS] = {
-  /* Layout */
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
+#define CHAR_0 '0'
+#define CHAR_9 '9'
+#define CHAR_SEND '#'
+#define MAX_DIGITS 9
+
+char keys[NUM_ROWS][NUM_COLS] = {{'1','2','3','A'},  {'4','5','6','B'},  {'7','8','9','C'},  {'*','0','#','D'}};  /* Layout */
 byte rowPins[NUM_ROWS] = {D0, D1, D2, D3}; /* Configuración de pines del numpad */
 byte colPins[NUM_COLS] = {D4, D5, D6, D7}; /* Configuración de pines del numpad */
 Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, NUM_ROWS, NUM_COLS); /* Instancia de la clase keypad */
 
+Infra objInfra;
+char *strTopicPub = "II3/ESP002/pub/cara002"; /* topic principal para publicar contenido y lastwill */
+char *strTopicCmd = "II3/ESP002/cmd/cara002"; /* topic para recibir peticiones de comando */
 
-void setup(){
-  /* Setup de la infraestructura */
-  objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
-  //objInfra.mqttTopicsSub[TOPIC_NUM_CFG] = strTopicCfg;
-  objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
-  objInfra.Setup(mqttCallback);
-}
-
-
+/* VARIABLES GLOBALES */
 int ans = NULL; /* Resupuesta del acertijo */
 
+/* --------------------------------------------------------------------------------------- */
 
-void mqttCallback(char* topic, byte* payload, unsigned int length){
-  /* Procesa los mensajes enviados por Node-red */
+/* FUNCIONES */
+void mqttCallback(char* topic, byte* payload, unsigned int length){  /* Procesa los mensajes enviados a través Node-red */
   char *mensaje = (char *)malloc(length+1);
   strncpy(mensaje, (char*)payload, length);
   mensaje[length]='\0';
@@ -58,17 +34,18 @@ void mqttCallback(char* topic, byte* payload, unsigned int length){
 }
 
 
-int countDigit(int number){
-  /* Calcula el número de dígitos que tiene un número cualquiera */
+int countDigit(int number){  /* Calcula el número de dígitos que tiene un número cualquiera */
   return int(log10(number) + 1);
 }
 
+void setup(){
+  /* Setup de la infraestructura */
+  objInfra.mqttTopicsPub[TOPIC_MAIN] = strTopicPub;
+  objInfra.mqttTopicsSub[TOPIC_NUM_CMD] = strTopicCmd;
+  objInfra.Setup(mqttCallback);
+}
 
-#define CHAR_0 '0'
-#define CHAR_9 '9'
-#define CHAR_SEND '#'
-#define MAX_DIGITS 9
-
+/* --------------------------------------------------------------------------------------- */
 
 void loop(){
   /* variables */
@@ -83,18 +60,15 @@ void loop(){
 
   char key = myKeypad.getKey(); /* Recibo una tecla del numpad */
   
-  if ((key == CHAR_SEND) && (ans == number)){
-    /* si se pulsa enviar y la respuesta es correcta */
+  if ((key == CHAR_SEND) && (ans == number)){    /* si se pulsa enviar y la respuesta es correcta */
     objInfra.ReportSuccess(" "); /* Actualiza el estado a: "You won" */    
   }
-  else if ((key == CHAR_SEND) && (ans != number)){
-    /* si se pulsa enviar y la respuesta es incorrecta */
+  else if ((key == CHAR_SEND) && (ans != number)){    /* si se pulsa enviar y la respuesta es incorrecta */
     objInfra.ReportFailure(" "); /* Actualiza el estado a: "Failure" y suma un intento */
     strcpy(strDigits,"");        /* Limpio el string en Node-Red */
     number = 0; /* Reinicio el número */
   }
-  else if (key >= CHAR_0 && key <= CHAR_9){
-    /* Envía el número a Node-Red */
+  else if (key >= CHAR_0 && key <= CHAR_9){    /* Envía el número a Node-Red */
     if(countDigit(number) < MAX_DIGITS){
       /* Evita el overflow */
       number = 10*number + (key-48); /* Concateno dígito a digíto para formar un número */
--- la-caja/arduino/cara004/cara004.ino
+++ la-caja/arduino/cara004/cara004.ino
@@ -1,67 +1,50 @@
-/* Librerías de uso generales*/
-#include <array>
-#include <string.h>
-#include <Wire.h>
-
-/* Libreria convertidor A/D ADS1015 */
-#include <Adafruit_ADS1X15.h>
-
-/* Librerías Pantalla Oled SSD1306 */
-#include <Adafruit_SSD1306.h>
+/* --------------------------------------------------------------------------------------- */
+#include <Adafruit_ADS1X15.h> /* Libreria convertidor A/D ADS1015 */
+#include <Adafruit_SSD1306.h> /* Librerías Pantalla Oled SSD1306 */
 #include <Adafruit_GFX.h>
-
-/* Librería general de la caja  */
-#include <infra.h>
-
-/* Librería de cosecha propia de la caja*/
-#include <Joystick.h>
-
+#include <infra.h>  /* Declaración de Infra */
+#include <Joystick.h> /* Librería de cosecha propia de la caja*/
 #define x_channel 1
 #define y_channel 0
 #define SCREEN_WIDTH 128 /* Units in px */
 #define SCREEN_HEIGHT 64 /* Units in px */
+#define MESSAGE_SIZE_ 300
 
-
-/* Create display */
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-/* Create joystick */
-Joystick joystick(x_channel,y_channel);
-
-/* Create objInfra */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT); /* Create display */
+Joystick joystick(x_channel,y_channel); /* Create joystick */
 Infra objInfra;
 char *strTopicPub = "II3/ESP004/pub/cara004"; // topic principal para publicar contenido y lastwill
 char *strTopicCmd = "II3/ESP004/cmd/cara004"; // topic para recibir peticiones de comando
 
-
-/* Resupuesta del acertijo */
-int ans = NULL;
+/* VARIABLES GLOBALES */
+int ans = NULL; /* Resupuesta del acertijo */
 String hour = "00:00";
-
-#define MESSAGE_SIZE_ 300
 StaticJsonDocument<MESSAGE_SIZE_> json_recibido;
 
-void mqttCallback(char* topic, byte* payload, unsigned int length)
-{
-  String command, commandValue; 
+
+/* --------------------------------------------------------------------------------------- */
+
+/* FUNCIONES */
+void mqttCallback(char* topic, byte* payload, unsigned int length){ /* Procesa los mensajes enviados a través Node-red */
+
   char *mensaje = (char *)malloc(length+1);
   strncpy(mensaje, (char*)payload, length);
   mensaje[length]='\0';
-  Serial.printf("Mensaje recibido [%s] %s\n", topic, mensaje);
 
-  if (strcmp(topic, strTopicCmd)==0)
-    {
+  if (strcmp(topic, strTopicCmd)==0){
       deserializeJson(json_recibido,mensaje);
       hour = json_recibido["content"].as<String>(); // leo de JSON
       ans = json_recibido["ans"]; // leo de JSON
       objInfra.ReportStart(NULL);
     }
-  
   free(mensaje);
 }
 
+int countDigit(int number){ /* Calcula el número de dígitos que tiene un número cualquiera */
+  return int(log10(number) + 1);
+}
+
 void setup(void){
-  
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     while (true);
   }
@@ -82,11 +65,7 @@ void setup(void){
   display.display();
 }
 
-int countDigit(int number){
-  /* Calcula el número de dígitos que tiene un número cualquiera */
-  return int(log10(number) + 1);
-}
-
+/* --------------------------------------------------------------------------------------- */
 
 void loop(){
   /* variables */
